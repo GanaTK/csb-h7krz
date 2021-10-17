a3e5fd1cadba6d4d1e89cd4658e4b39e8610e39c
Code Sandbox <hello@codesandbox.io>
2021-10-17 17:22:39 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.357916e2b7962a608752cd9a28173f19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93f36e627dcdd5838451",
+    "url": "https://ganatk.github.io/csb-h7krz/static/css/main.40600a88.chunk.css"
+  },
+  {
+    "revision": "93f36e627dcdd5838451",
+    "url": "https://ganatk.github.io/csb-h7krz/static/js/main.75ddab36.chunk.js"
+  },
+  {
+    "revision": "b31b33458db09ee6b5e8",
+    "url": "https://ganatk.github.io/csb-h7krz/static/js/runtime~main.629bd2ee.js"
+  },
+  {
+    "revision": "5fee0f108ebd2528a255",
+    "url": "https://ganatk.github.io/csb-h7krz/static/js/2.709b3c31.chunk.js"
+  },
+  {
+    "revision": "7e0f853b905949eb8f1aa24551cafaff",
+    "url": "https://ganatk.github.io/csb-h7krz/index.html"
+  }
+];
